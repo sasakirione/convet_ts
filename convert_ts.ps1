4d33c3f2231ad51d772dfa1c753e9d5f32c3365b
sasakirione <sasakirione@gmail.com>
2023-05-26 23:40:40 +0900

    Update: 設定ファイルの外部化

--- convert_ts.ps1
+++ convert_ts.ps1
@@ -1,16 +1,40 @@
-$logFile = "C:/path/to/logfile.log"  # ログファイルのパスを指定
+$logFile = "../log/logfile.log"  # ログファイルのパスを指定
 
 $nw = Get-Date -Format o
 $dt = [DateTime]$nw
 $dt = $dt.AddDays(-2)
 $nw = $dt.ToString("o")
 
-$file_list = Get-ChildItem -Path 'D:/録画未整理/*.ts'
-$file_list_limit_ten = $file_list[0..19]
+WriteLog -message "処理開始"
 
-foreach($f in $file_list_limit_ten){
+# yaml設定ファイルを読み込む
+$yaml = Get-Content -Path "../config/config.yaml" | ConvertFrom-Yaml
+$source_path = $yaml.source_path + "*.ts"
+$process_count = $yaml.process_count
+
+# 対象ファイル一覧を取得する
+$file_list = Get-ChildItem -Path $source_path
+$file_list_count = $file_list.Count
+
+# 対象ファイルが多すぎる場合に規定の数に絞る
+# 対象がない場合
+$target_file_list = @()
+if ($file_list_count -eq 0){
+    WriteLog -message "処理対象のファイルがありません"
+    exit
+# 対象が20個以下の場合
+} elseif ($file_list_count -le $process_count){
+    $target_file_list = $file_list 
+} else {
+    $index = $process_count - 1
+    $target_file_list = $file_list[0..$index]
+}
+
+foreach($f in $target_file_list){
     ConvertTo-MP4 -inputPath $f.FullName -targetTime $nw
 }
+WriteLog -message "処理終了"
+
 
 function WriteLog($message){
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
